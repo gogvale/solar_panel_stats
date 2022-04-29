d3044818ed42daf6de7d698fe735b1916c752c31
Gabriel Oliveira <gogvale2011@gmail.com>
2022-04-28 21:13:04 -0500

    finally fix timezone

--- app/models/measurement.rb
+++ app/models/measurement.rb
@@ -7,20 +7,20 @@ class Measurement < ApplicationRecord
   private
 
   def adjust_created
-    offset = time_difference.hours
-    created_at -= offset
-    updated_at -= offset
+    offset = time_difference
+    self.created_at = offset
+    self.updated_at = offset
   end
 
   def adjust_saved
-    offset = time_difference.hours
-    updated_at -= offset
+    offset = time_difference
+    self.updated_at = offset
   end
 
   def time_difference
     time1 = Time.zone.now.in_time_zone('UTC')
     time2 = Time.zone.now.in_time_zone('Monterrey')
     time_difference_in_seconds = time2.utc_offset - time1.utc_offset
-    (time_difference_in_seconds / 60 / 60).abs
+    time1 - (time_difference_in_seconds / 60 / 60).abs.hours
   end
 end
