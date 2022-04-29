3030c263f07cfdf331dc57f0d5f20d58da9cee94
Gabriel Oliveira <gabriel.oliveira@axented.com>
2022-04-28 22:18:31 -0500

    Reformat queries

--- app/controllers/measurements_controller.rb
+++ app/controllers/measurements_controller.rb
@@ -1,13 +1,25 @@
+# frozen_string_literal: true
+
 class MeasurementsController < ApplicationController
   def dashboard
     @todays_measure = Measurement.where(created_at: 1.day.ago..nil)
-                                 .group_by_minute(:created_at, format: "%l%P")
+                                 .group_by_minute(:created_at, format: '%l%P')
                                  .sum(:current_power)
     @weekly_measure = Measurement.where(created_at: 1.week.ago..nil)
-                                 .group_by_day_of_week(:created_at, format: "%a")
-                                 .sum(:current_power)
-    @yearly_measure = Measurement.where(created_at: 1.year.ago..nil)
-                                 .group_by_month(:created_at, format: "%b")
-                                 .sum(:current_power)
+                                 .group_by_day_of_week(:created_at, format: '%a')
+                                 .maximum(:created_at)
+                                 .map { |key, i| { key => Measurement.find_by(created_at: i)&.yield_today } }
+                                 .reduce({}, :merge)
+    @yearly_measure = (0..12).to_a.reverse.map do |j|
+      time = j.months.ago
+      { time.strftime('%b') => Measurement.where(created_at: time.beginning_of_month..time.end_of_month)
+                                          .group_by_day_of_week(:created_at, format: '%a')
+                                          .maximum(:created_at)
+                                          .map { |key, i| { key => Measurement.find_by(created_at: i)&.yield_today } }
+                                          .reduce({}, :merge)
+                                          .compact_blank
+                                          .values
+                                          .sum }
+    end.reduce({}, :merge)
   end
 end
